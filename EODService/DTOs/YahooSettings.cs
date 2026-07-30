2c06c2deade99cfbc8ec1cb575593dda4ed43549
mostafa.raafat.inter <mostafa.raafat.inter@TEST-IT>
2026-07-30 11:57:41 +0300

    Git Init ALL

---
+++ EODService/DTOs/YahooSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EODService.DTOs
+{
+    public class YahooSettings
+    {
+        public string BaseUrl { get; set; }
+        public string Endpoint{ get; set; }
+        public string Interval { get; set; }
+        public string Range { get; set; }
+        public YahooSettings(string baseUrl, string endpoint, string interval, string range)
+        {
+            BaseUrl = baseUrl;
+            Endpoint = endpoint;
+            Interval = interval;
+            Range = range;
+        }
+    }
+}
---
+++ EODService/DTOs/YahooSettingsMapper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EODService.DTOs
+{
+    public  class YahooSettingsMapper
+    {
+        public YahooSettings MapToYahooSettings()
+        {
+            var configuration = new ConfigurationBuilder()
+                .
+                .AddJsonFile("appsettings.json")
+                .Build();
+            return new YahooSettings(baseUrl, endpoint, interval, range);
+        }
+    }
+}
