7cce55bf456d5f5e76d342d30c473b4ca9c4c3fd
mostafa.raafat.inter <mostafa.raafat.inter@TEST-IT>
2026-08-02 14:50:47 +0300

    Adding Infrastructure layer with DB architecure set

---
+++ Infrastructure/Persistance/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using EODService.DTOs.EOD;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Persistance
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<EodData> EodDaily { get; set; }
+    }
+}
---
+++ Infrastructure/Persistance/AppDbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Oracle.EntityFrameworkCore.Internal;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Persistance
+{
+    public class AppDbContextFactory
+    {
+        public static AppDbContext Create(string ConnectionSettings)
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseOracle(ConnectionSettings)
+                .Options;
+
+            return new AppDbContext(options);
+        }
+    }
+}
