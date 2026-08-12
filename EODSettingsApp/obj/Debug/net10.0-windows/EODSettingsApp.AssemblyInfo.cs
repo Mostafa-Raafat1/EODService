ed56858635208bbbd89954402d489f31544e0db7
moneer.magdi.intern <moneer.magdi.intern@SEC-METER>
2026-08-12 16:55:10 +0300

    youssef add-form merge fixed

--- EODSettingsApp/obj/Debug/net10.0-windows/EODSettingsApp.AssemblyInfo.cs
+++ EODSettingsApp/obj/Debug/net10.0-windows/EODSettingsApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EODServiceManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cbe11691c16700139bc7f338d585766d5450dd8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9d42e57fbb8558622efd9d035c43387769568eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("EODServiceManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EODServiceManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
