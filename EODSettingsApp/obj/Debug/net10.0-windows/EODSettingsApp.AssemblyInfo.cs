5f8c3b6ba27ecf4be78ef9fc83f7b4d52f2f36a2
youssef.azzab.intern <youssef.azzab.intern@AHMED-HATEM-INT>
2026-08-17 09:56:49 +0300

    fixed monier decryption and encryptionmerging from monier

--- EODSettingsApp/obj/Debug/net10.0-windows/EODSettingsApp.AssemblyInfo.cs
+++ EODSettingsApp/obj/Debug/net10.0-windows/EODSettingsApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EODServiceManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9f33e40376f0bd15cb244ea9446d8b381ab7cf2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3cf66bb049bee606eab5d2b0893804fca1e11ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("EODServiceManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EODServiceManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
